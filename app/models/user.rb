eb34a3f444716ec748a82f9a21d3eeddf056de28
bomute kim <kimbomte1996@icloud.com>
2022-08-22 22:29:08 +0900

    ペアワークのログイン機能を実装

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true,
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_secure_password
+  before_validation { email.downcase! }
+end
